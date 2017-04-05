29c0c2bd747e860827cbcc76909cb797ce1dab58
Mozartted <mozart.osita@gmail.com>
2017-04-05 02:16:19 +0100

    Controllers defined

---
+++ controllers/appointment.controller.js
@@ -0,0 +1,41 @@
+var Appointment = require('../models/appointments.model.js');
+
+
+
+modules.exports = {
+    getAll:function(req,res){
+
+    },
+
+    createAppointment:function(req,res){
+
+    },
+
+    getAppointmentDetails:function(req,res){
+
+    },
+
+    updateAppointment:function(req,res){
+
+    },
+
+    deleteAppointment:function(req,res){
+
+    }
+
+}
+
+
+// app.post('/api/create/task', Task.createTask);
+//   app.post('/api/create/appointment',Appointment.createAppointment);
+
+
+//   app.get('/api/task/:id', Task.getTaskDetails);
+//   app.get('/api/appointment/:id', Appointment.getAppointmentDetails);
+
+
+//   app.put('/api/task/:id',Task.updateTask);
+//   app.put('/api/appointment/:id',Appointment.updateAppointment);
+
+//   app.delete('/api/task/:id',Task.deleteTask);
+//   app.delete('/api/appointment/:id',Appointment.deleteAppointment);
---
+++ controllers/task.controller.js
@@ -0,0 +1,26 @@
+var Task = require('../models/tasks.model.js');
+
+
+
+modules.exports = {
+    getAll:function(req,res){
+
+    },
+
+    createTask:function(req,res){
+
+    },
+
+    getTaskDetails:function(req,res){
+
+    },
+
+    updateTask:function(req,res){
+
+    },
+
+    deleteTask:function(req,res){
+
+    }
+
+}
