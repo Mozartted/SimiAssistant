0a022d8e555c2ef3c824ac9ecc8fb7b432cdc08a
Chibuike Emmauel Osita <mozart.osita@gmail.com>
2017-04-10 20:02:33 +0100

    Modifications adding Cron job

--- server/config.js
+++ server/config.js
@@ -1,5 +1,9 @@
 require('dotenv').load();
+var mongoose = require('mongoose');
 var cfg = {};
+
+cfg.db = mongoose.connection;
+mongoose.connect(cfg.mongoUrl);
 
 // HTTP Port to run our web application
 cfg.port = process.env.PORT || 3000;
@@ -11,5 +15,14 @@ cfg.PhoneNumber = process.env.PHONE_NUMBER;
 
 cfg.mongoUrl = process.env.MONGOLAB_URI || process.env.MONGO_URL
 
+
 // Export configuration object
-module.exports = cfg;
+module.exports = {
+    cfg:cfg,
+    dbConnect:function(){
+        cfg.db.on('error', console.error.bind( console, 'MongoDB Connection Error. Please make sure that MongoDB is running.'));
+        cfg.db.once('open', function callback(){
+            console.log('Yourtube db opened');
+        });
+    }
+} 
---
+++ workers/notificationWorkers.js
@@ -0,0 +1,11 @@
+var Appointment = require('../server/models/appointments.model')
+
+var notificationWorkerFactory =  function(){
+  return {
+    run: function(){
+      Appointment.sendNotifications();
+    }
+  };
+};
+
+module.exports = notificationWorkerFactory();
